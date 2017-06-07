bd9fb2db25e8df1622450eb976333ec3709f658d
ANGEL <angelxd84130@gmail.com>
2017-06-07 22:47:11 +0800

    Make a notification on message received.
    Firebase payload change to "data only", and make notification by ourselves.

--- app/src/main/java/com/example/angel/chatroom/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/angel/chatroom/MyFirebaseMessagingService.java
@@ -1,5 +1,8 @@
 package com.example.angel.chatroom;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -13,6 +16,14 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Log.d("FCM", "onMessageReceived:"+remoteMessage.getFrom());
+        // Create notification builder.
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(remoteMessage.getData().get("test"))
+                .setContentText("");
+        // Set notification to system tray.
+        NotificationManager mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotifyMgr.notify(1, mBuilder.build());
     }
     
 }
